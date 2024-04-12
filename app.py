3f7470d57fd24ce0a10534ed5acbc3dd0b193661
hadinawfal <hadinawfal.123@gmail.com>
2024-04-12 20:56:16 +0300

    Initial commit: Added Folder

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+    
+@app.route("/about")
+def about():
+     return render_template('about.html')
+
+@app.route("/DES")
+def DES():
+     return render_template('DES.html')
+
+@app.route("/AES-CCM")
+def AES():
+     return render_template('AES.html')
+
+@app.route("/Polyari")
+def Polyari():
+     return render_template('Polyari.html')
